ad8a04c48fa910ade17b44f7a3040bab1fe1c059
kenichi.nakazawa <kenichi.nakazawa@eys-style.com>
2019-10-05 19:16:25 +0900

    ブロックの描画まで

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import tkinter #GUI描画
+import tkinter # GUI描画
 screen_width = 600 # 画面の幅
 screen_height = 400 # 画面の高さ
+blocks_height = 200 # ブロック部分の高さ
 fps = 1000 // 30 # 描画間隔
+block_columns = 10 # ブロックの横の数
+block_rows = 4 # ブロックの縦の数
+colors =['red', 'yellow', 'blue'] #ブロックの色
+
 window = tkinter.Tk() # ウインドウ作成
 window.title('ブロックくずし')
 canvas = tkinter.Canvas(window, width=screen_width, height=screen_height) # 描画領域作成
@@ -19,7 +24,7 @@ class Ball:
   def draw(self, canvas):
     canvas.create_oval(
         self.x - self.r, self.y - self.r,
-      self.x + self.r, self.y + self.r,
+        self.x + self.r, self.y + self.r,
         fill = 'green')
 
   def move(self):
@@ -30,11 +35,43 @@ class Ball:
     if self.y <= self.r or self.y + self.r >= screen_height:
       self.dy = -self.dy
 
+class Block:
+  def __init__(self, row, column, life = 1):
+    self.row = row
+    self.column = column
+    self.life = life
+
+  def x(self):
+    return self.column * screen_width // block_columns
+  
+  def y(self):
+    return self.row * blocks_height // block_rows
+  
+  def x2(self):
+    return self.x() + screen_width // block_columns - 1
+  
+  def y2(self):
+    return self.y() + blocks_height // block_rows - 1
+
+  def color(self):
+    return colors[self.life]
+
+  def draw(self, canvas):
+    canvas.create_rectangle(self.x(), self.y(), self.x2(), self.y2(), fill=self.color())
+
 ball = Ball()
+blocks = []
+for row in range(0, block_rows):
+  blocks.append([])
+  for column in range(0, block_columns):
+    blocks[-1].append(Block(row, column))    
 
 def gameloop():
   ball.move() # ボール移動
   canvas.delete('all') # 画面クリア
+  for row in blocks: # ブロックの描画
+    for block in row:
+      block.draw(canvas)
   ball.draw(canvas) # ボールの描画
   window.after(fps, gameloop) # 繰り返し
 
