bfd5f6c3a0c45aab9d5ccb088e4ba495a0746987
kenichi.nakazawa <kenichi.nakazawa@eys-style.com>
2019-10-05 18:21:15 +0900

    ボールを動かすところまで

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ canvas.pack() # ウインドウサイズを描画領域に合わせる
 class Ball:
 
   def __init__(self):
-    self.r = 10 #半径
-    self.x = screen_width / 2 - self.r #初期横位置
-    self.y = screen_height / 2 - self.r #初期縦位置
+    self.r = 10 # 半径
+    self.x = screen_width / 2 - self.r # 初期横位置
+    self.y = screen_height / 2 - self.r # 初期縦位置
+    self.dx = 4 # 初期横移動量
+    self.dy = 3 # 初期縦移動量
 
   def draw(self, canvas):
     canvas.create_oval(
@@ -20,9 +22,18 @@ class Ball:
       self.x + self.r, self.y + self.r,
         fill = 'green')
 
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+    if self.x <= self.r or self.x + self.r >= screen_width:
+      self.dx = -self.dx
+    if self.y <= self.r or self.y + self.r >= screen_height:
+      self.dy = -self.dy
+
 ball = Ball()
 
 def gameloop():
+  ball.move() # ボール移動
   canvas.delete('all') # 画面クリア
   ball.draw(canvas) # ボールの描画
   window.after(fps, gameloop) # 繰り返し
